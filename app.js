c4788f876b50f4ef6c4be5a180430ade158f86ec
WinstonAdams <apollo07291@gmail.com>
2023-10-20 21:01:29 +0800

    chore: modify header

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
   res.locals.warning_messages = req.flash('warning_messages')
+  res.locals.user = req.user
   next()
 })
 
