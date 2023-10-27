759b02e4c3671745e865d9055686c3f4993dba6c
WinstonAdams <apollo07291@gmail.com>
2023-10-27 16:40:38 +0800

    feat: add admin seed

--- seeders/20231017141623-users-seed.js
+++ seeders/20231017141623-users-seed.js
@@ -4,21 +4,31 @@ const bcrypt = require('bcryptjs')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
+      name: 'admin',
+      email: 'admin@example.com',
+      password: await bcrypt.hash('123456', 10),
+      is_admin: true,
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
       name: 'user1',
       email: 'user1@example.com',
       password: await bcrypt.hash('123456', 10),
+      is_admin: false,
       created_at: new Date(),
       updated_at: new Date()
     }, {
       name: 'user2',
       email: 'user2@example.com',
       password: await bcrypt.hash('123456', 10),
+      is_admin: false,
       created_at: new Date(),
       updated_at: new Date()
     }, {
       name: 'user3',
       email: 'user3@example.com',
       password: await bcrypt.hash('123456', 10),
+      is_admin: false,
       created_at: new Date(),
       updated_at: new Date()
     }], {})
